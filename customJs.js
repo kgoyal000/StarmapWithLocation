f24862fbfb20b00a06d87a4348c6d9aa7141bcaf
kgoyal000 <62390711+kgoyal000@users.noreply.github.com>
2020-06-18 22:10:58 +0530

    Update customJs.js

--- customJs.js
+++ customJs.js
@@ -7,7 +7,7 @@ function trigger_change_event(element_id){
 }
 
 //Color changes
-document.getElementById("comp-kbkfxefvlink").addEventListener('click',function(){
+//document.getElementById("comp-kbkfxefvlink").addEventListener('click',function(){
 var showGrid = document.getElementById("toggle-switch-comp-kbkfry6q").checked;
 var showConstellations = document.getElementById("toggle-switch-comp-kbkfu2mm").checked;
 var color;
@@ -115,18 +115,18 @@ planets: {
       }
     }
   })
-});
-/*for constellations
+//});
+//for constellations
 document.getElementById("toggle-switch-comp-kbkfu2mm").addEventListener('change',function(){
     var showConst = document.getElementById("toggle-switch-comp-kbkfu2mm").checked;
     document.getElementById("constellations-lines").checked = showConst;
     trigger_change_event("constellations-lines")
 });
 
-for Grid
+//for Grid
 document.getElementById("toggle-switch-comp-kbkfry6q").addEventListener('change',function(){
        var showGrid = document.getElementById("toggle-switch-comp-kbkfry6q").checked;
     document.getElementById("lines-graticule-show").checked = showGrid;
     trigger_change_event("lines-graticule-show");
 });
-*/
+
