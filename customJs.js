ad0bce81993f80411d2b14944636e16f6d234869
kgoyal000 <62390711+kgoyal000@users.noreply.github.com>
2020-06-23 17:42:16 +0530

    Update customJs.js

--- customJs.js
+++ customJs.js
@@ -355,13 +355,23 @@ document.getElementById('comp-kbkfxefv').addEventListener('click',function(){
 
     $('#cover-spin').show(0)
     submit_starForm();
-    document.querySelector('textarea._2ORQo').value = details; 
-    $('textarea._2ORQo').on('focus',function(){
-        document.querySelector('textarea._2ORQo').value = details; 
-     })
-              $("textarea._2ORQo").focus();
-     $("textarea._2ORQo").click();
-        console.log(document.querySelector('textarea._2ORQo').value);
+    function setNativeValue(element, value) {
+    let lastValue = element.value;
+    element.value = value;
+    let event = new Event("input", { target: element, bubbles: true });
+    // React 15
+    event.simulated = true;
+    // React 16
+    let tracker = element._valueTracker;
+    if (tracker) {
+        tracker.setValue(lastValue);
+    }
+    element.dispatchEvent(event);
+}
+
+var input = document.querySelector("textarea._2ORQo");
+setNativeValue(input, details);
+console.log(document.querySelector('textarea._2ORQo').value);
 
     document.getElementsByClassName('StatesButton928128992--root AddToCartButton3061789056--addToCartButton')[0].click()
 });
