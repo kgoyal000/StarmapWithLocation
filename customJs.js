98cd39b86fa4741f256e8dc5056285a386d5181b
kgoyal000 <62390711+kgoyal000@users.noreply.github.com>
2020-06-23 15:34:17 +0530

    Update customJs.js

--- customJs.js
+++ customJs.js
@@ -338,10 +338,9 @@ setTimeout(() => {
             }
         document.getElementsByClassName('_2ORQo')[0].classList.add('_1dGXc');
         document.querySelector('textarea._2ORQo').value = address+"\n"+day+"/"+month+"/"+year+"\n"+hrs+":"+min+"\n"+name+"\n"+message+"\n"+order;
-        element =  document.querySelector('textarea._2ORQo')
-        var event = new Event('change');
-        // Dispatch it.
-        element.dispatchEvent(event);
+        $('textarea._2ORQo').on('focus',function(){
+        document.querySelector('textarea._2ORQo').value = address+"\n"+day+"/"+month+"/"+year+"\n"+hrs+":"+min+"\n"+name+"\n"+message+"\n"+order;
+        })
         console.log(document.querySelector('textarea._2ORQo').value);
         document.getElementById("json_data").value = JSON.stringify(json);
         $("#svg_element").remove();
