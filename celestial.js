069ec3066483f5674bf7f49e3156841556d6d74a
kgoyal000 <62390711+kgoyal000@users.noreply.github.com>
2021-12-01 15:57:17 +0530

    Update celestial.js

--- celestial.js
+++ celestial.js
@@ -778,7 +778,7 @@
    }, t.getData = L, t.getPoint = q, t.getPlanet = O;
    var W = {};
    var V = {
-      width: 280,
+      width: 480,
       projection: "airy",
       transform: "equatorial",
       center: null,
